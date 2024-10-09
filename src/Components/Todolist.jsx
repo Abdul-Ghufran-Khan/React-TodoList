706c44ce644b0ddf3a78cb4be8a57fb69fa87317
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-10-09 23:08:47 +0500

    Commit

--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -7,8 +7,7 @@ export default function Todolist({ todos, onDelete }) {
         <div key={todo.id} className="flex px-5 py-3 rounded-md my-2 justify-between items-center bg-slate-200">
           <h3>{todo.title}</h3>
           <div className=" mx-0">
-            <MdDelete 
-            className="text-3xl hover:text-red-700" onClick={() => onDelete(todo.id)}/>
+            <MdDelete className="text-3xl hover:text-red-700" onClick={() => onDelete(todo.id)}/>
           </div>
         </div>
       ))}
